857c900eed6b16e2f3a7095852145566bf2001ec
alexClifton <oooalexoooo@gmail.com>
2021-12-23 22:02:23 -0800

    add onClick navigation to sub pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
 import AboutPage from "./pages/aboutpage/aboutpage.component.jsx";
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 
 function App() {
+  let navigate = useNavigate();
   return (
     <div className="App">
       <header className="App-header">
-        <h1>ZTM</h1>
+        <h1 onClick={() => navigate("/")}>ZTM</h1>
       </header>
       <Routes>
         <Route exact path={"/"} element={<HomePage />} />
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./menu-item.styles.scss";
+import { useNavigate } from "react-router-dom";
+
 function MenuItem({ title }) {
+  let navigate = useNavigate();
   return (
     <div className="menu-item">
       <div className="content">
         <h1 className="title">{title}</h1>
-        <h1 className="subtitle">SHOP</h1>
+        <h1 onClick={() => navigate(`/${title}`)} className="subtitle">
+          SHOP
+        </h1>
       </div>
     </div>
   );
