5d9425086bdcbe94efedd1a6b023e65d4c6956a4
alexClifton <oooalexoooo@gmail.com>
2021-12-23 21:06:20 -0800

    create about page route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
+import AboutPage from "./pages/aboutpage/aboutpage.component.jsx";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       </header>
       <Routes>
         <Route exact path={"/"} element={<HomePage />} />
+        <Route exact path={"/about"} element={<AboutPage />} />
       </Routes>
     </div>
   );
