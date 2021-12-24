d7b2e8389af2e70e1468dcc30ca237ab8d873ec2
alexClifton <oooalexoooo@gmail.com>
2021-12-23 20:56:01 -0800

    create page templates for about and shop pages

---
+++ src/pages/aboutpage/aboutpage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./aboutpage.styles.scss";
+
+const AboutPage = () => {
+  return (
+    <div className="aboutpage">
+      <h1>ABOUT PAGE</h1>
+    </div>
+  );
+};
+
+export default AboutPage;
---
+++ src/pages/shoppage/shoppage.component.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./shoppage.styles.scss";
+
+const ShopPage = () => {
+  return (
+    <div className="shoppage">
+      <h1>SHOP PAGE</h1>
+    </div>
+  );
+};
+
+export default ShopPage;
